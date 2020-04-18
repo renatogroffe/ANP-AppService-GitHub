aac1ceacc29553ae5e909457deed14c1eb38a63a
Renato Groffe <renato.groffe@yahoo.com.br>
2020-04-18 10:02:15 -0300

    Commit via GitHub

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Teste",
+                    Local = "Teste: Deployment Center + GitHub",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
